f440b4782f3a262ddb020259d95e971f3c238ad1
Lexia Ledaga <ambaba-lexia.ledaga-leounda@epitech.eu>
2019-11-15 14:42:20 -0500

    Fix my_printf.c

--- lib/my/my_printf.c
+++ lib/my/my_printf.c
@@ -15,11 +15,10 @@ int flags(char const *str, va_list ap, int c)
 
     void(*function[10])() = {print_s, print_d, print_i, print_u, print_X,
         print_x, print_c, print_o, print_b, print};
-        for (int z = 0; flags[z] != '\0'; z++) {
-            if (flags[z] == str[c])
-                function[z](ap);
-
-        }
+    for (int z = 0; flags[z] != '\0'; z++) {
+        if (flags[z] == str[c])
+               function[z](ap);
+    }
 }
 
 int my_printf(const char *format, ...)
